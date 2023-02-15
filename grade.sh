96ac61eae3f047598957c0a1041d07b8ecdfb043
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2023-02-15 15:10:43 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -3,3 +3,16 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+
+if[-e GradeServer]
+    then
+    echo "GradeServer file submission found"
+
+    else
+    "Gradeserver file not found. Please name your submitted file correctly"
+    exit(3);
+
+cp ../TestListExamples.java ./;
+
+
+
