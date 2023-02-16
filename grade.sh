76e915183bf6abe2b7becfd3c205bf45d74744e0
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2023-02-15 16:37:36 -0800

    Complete grade.sh

--- grade.sh
+++ grade.sh
@@ -1,18 +1,54 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if[-e GradeServer]
-    then
-    echo "GradeServer file submission found"
+# run command:
+#bash grade.sh https://github.com/some-username/some-repo-name
+cd student-submission
+#echo 'Finished cd'
 
-    else
-    "Gradeserver file not found. Please name your submitted file correctly"
-    exit(3);
+if [ -f ListExamples.java ]
+then
+    echo "ListExamples file submission found"
+else
+    echo "ListExamples file not found. Please name your submitted file correctly"
+    exit
+fi
 
-cp ../TestListExamples.java ./;
+cp ../TestListExamples.java ./student-submission;
+#cp ../ ./student-submission/TestListExamples.java;
+
+
+javac -cp $CPATH *.java
+
+if [ $? -eq 1 ]
+then
+    echo "Compile error"
+    exit
+else
+    echo "Compile successful"
+fi
+
+java -cp ".;../lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples >results.txt 2>&1
+
+if [ $? -eq 0 ]
+then 
+   echo "Passed all tests, full credit"
+else
+   cat results.txt | grep -E -w  "Failures:"
+fi
+
+
+
+
+
+
+
+
+
+
 
 
 
